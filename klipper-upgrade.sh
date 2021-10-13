c6d5f47796d44387324f0d0587ae58975e4d8b39
Grant Rodgers <grantr@gmail.com>
2021-10-13 12:32:02 -0700

    Some qtpy stuff (unused), M601, load filament

--- klipper-upgrade.sh
+++ klipper-upgrade.sh
@@ -2,12 +2,14 @@
 
 set -xeuo pipefail
 
+pushd $(dirname "${BASH_SOURCE[0]}")/klipper
+
 rm -f .config
 ln -s pi.config .config
 make clean
 make -j5
 sudo systemctl stop klipper_mcu
-sudo cp out/klipper.elf /usr/local/bin
+sudo scripts/flash-linux.sh
 sudo systemctl start klipper_mcu
 
 rm -f .config
@@ -19,5 +21,12 @@ scripts/flash-sdcard.sh /dev/ttyACM0 fysetc-spider-v1
 sudo systemctl start klipper
 
 # TODO QT Py
+rm -f .config
+ln -s qtpy.config .config
+make clean
+make -j5
+sudo systemctl stop klipper
+make flash FLASH_DEVICE=/dev/ttyACM1
 
 rm -f .config
+popd
